9b0d4c6ffaf7601d27a905039ed8ff51343d6236
Your Name <you@example.com>
2024-03-03 13:48:20 +0200

    The final version of the class, contains documentations

--- file_reader.py
+++ file_reader.py
@@ -1,18 +1,33 @@
+"""
+ Author: Elian
+ Purpose: A file reader.
+ Date: 03.03.2024
+"""
 import os.path
 from abc import abstractmethod
 
 
 class FileReader:
+    """A file reader."""
     def __init__(self, file_path):
         if os.path.isfile(file_path):
             self.file_path = file_path
 
     def size(self):
+        """
+        Returns the file's size.
+        :return: The file size.
+        """
         return os.stat(self.file_path).st_size
 
     def name(self):
+        """
+        Return the file name.
+        :return: File name.
+        """
         return os.path.splitext(self.file_path)[0]
     
     @abstractmethod
     def read(self):
+        """Abstract method."""
         pass
